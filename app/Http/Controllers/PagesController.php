fb7ae57cebb01a5c7acbce696c3e339f9152de90
andela-sakande <surajudeen.akande@andela.com>
2016-05-06 18:47:45 +0000

    Select 3 cakes for landing page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,7 +10,7 @@ class PagesController extends Controller
 {
     public function index()
     {
-        $cakes = Product::all();
+        $cakes = Product::all()->take(3);
 
         return view('landing', compact('cakes'));
     }
