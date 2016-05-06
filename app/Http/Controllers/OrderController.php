76870fd55298b838430bc609d335315e60bb74b4
andela-sakande <surajudeen.akande@andela.com>
2016-05-06 23:32:31 +0000

    Add 404 and login

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace CakeDeal\Http\Controllers;
 
+use Auth;
 use CakeDeal\Order;
 use CakeDeal\Product;
 use Illuminate\Http\Request;
@@ -11,13 +12,23 @@ class OrderController extends Controller
 {
     public function index($id)
     {
+        dd($id);
         $order = Product::where('id', $id)->first();
 
         return view('app.show', compact('order'));
     }
 
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
+        $order = Order();
+        $order->user_id = Auth::user()->id;
+        $order->product_id = $id->id;
+        $order->quantity = $request->input('quantity');
+        $order->message = $request->input('message');
+        $order->delivery_date = $request->input('delivery-date');
 
+        $order->save();
+
+        return redirect()->to('/dashboard');
     }
 }
